b1321ed504a210b1840d7618967d97ab2741ac74
BandaShireesha <2511cs030486@mallareddyuniversity.ac.in>
2026-07-29 15:18:29 +0530

    Add files via upload

---
+++ Ecommercestring.java
@@ -0,0 +1,20 @@
+public class EcommerceString {
+   public EcommerceString() {
+   }
+
+   public static void main(String[] var0) {
+      String var1 = "Head Set";
+      String var2 = "Electronics";
+      String var3 = "Boat";
+      String var4 = "Chandu";
+      String var5 = "UPI";
+      String var6 = "Delivered";
+      System.out.println("===== Ecommerce order =====");
+      System.out.println("Customer Name : " + var4);
+      System.out.println("Product Name : " + var1);
+      System.out.println("Category : " + var2);
+      System.out.println("Brand : " + var3);
+      System.out.println("Payment Method : " + var5);
+      System.out.println("Order Status : " + var6);
+   }
+}
